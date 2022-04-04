a7313b37fcbbb44244bd2c2d16337b85f2d4aa49
Julia Patrin <papercrane@reversefold.com>
2022-04-03 23:00:52 -0700

    Make the pixel screen display an arrow Remove unused CPE code

--- CircuitPy_Adafruit/code.py
+++ CircuitPy_Adafruit/code.py
@@ -26,23 +26,63 @@ except OSError:
 
 MAIN = not WORK
 
-IS_CPE = "feather" not in board.board_id
+if MAIN:
+    try:
+        import adafruit_displayio_ssd1306
+        import adafruit_display_text.label
+        import displayio
+        import terminalio
 
-if IS_CPE:
-    button_a = digitalio.DigitalInOut(board.BUTTON_A)
-    button_a.switch_to_input(pull=digitalio.Pull.DOWN)
-    button_b = digitalio.DigitalInOut(board.BUTTON_B)
-    button_b.switch_to_input(pull=digitalio.Pull.DOWN)
-elif MAIN:
+        displayio.release_displays()
+        i2c = board.I2C()
+        display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
+        display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+    except RuntimeError as exc:
+        print("Error initializing display: %r" % (exc,))
+        display = None
+
+
+def update_display(text):
+    if not display:
+        return
+    # Make the display context
+    splash = displayio.Group()
+    display.show(splash)
+
+    color_bitmap = displayio.Bitmap(128, 32, 1)
+    color_palette = displayio.Palette(1)
+    color_palette[0] = 0xFFFFFF  # White
+
+    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+    splash.append(bg_sprite)
+
+    # Draw a smaller inner rectangle
+    inner_bitmap = displayio.Bitmap(118, 24, 1)
+    inner_palette = displayio.Palette(1)
+    inner_palette[0] = 0x000000  # Black
+    inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=4)
+    splash.append(inner_sprite)
+
+    # Draw a label
+    text_area = adafruit_display_text.label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=int(128 / 2 - len(text) / 2 * 6), y=15)
+    splash.append(text_area)
+
+
+if MAIN:
     button = digitalio.DigitalInOut(board.D5)
     button.switch_to_input(pull=digitalio.Pull.DOWN)
 
-if IS_CPE:
-    pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
-elif MAIN:
-    pixels = neopixel.NeoPixel(board.D6, 8 * 4, brightness=0.1, auto_write=False)
+
+if MAIN:
+    pixels = neopixel.NeoPixel(board.D6, 8 * 4, brightness=0.05, auto_write=False)
+    pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.05, auto_write=False)
 else:
     pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.05, auto_write=False)
+
+# switch_to_0 = digitalio.DigitalInOut(board.D12)
+# switch_to_0.switch_to_output()
+# switch_to_1 = digitalio.DigitalInOut(board.D13)
+# switch_to_1.switch_to_output()
 
 switch = digitalio.DigitalInOut(board.D4 if MAIN else board.D11)
 switch.switch_to_output()
@@ -75,8 +115,13 @@ def wheel(pos):
 
 last_out = 0
 timer = 0
-PERIOD = 150
+PERIOD = 100
 COLOR_RATIO = 0.5
+
+def mix_colors(color1, color2):
+    color = tuple(color1[i] * COLOR_RATIO + color2[i] * (1 - COLOR_RATIO) for i in (0, 1, 2))
+    return color
+
 def update_pixels(color):
     global last_out, timer
 
@@ -90,22 +135,31 @@ def update_pixels(color):
     #     print(">", ts, color)
     #     last_out = ts
     # pixels.fill(color)  #  * int(ts % 5 / 5.0)
-    for x in range(8):
+    for x in range(len(pixels) // 4):
         # col = tuple(int(v * (((x + 7.0 * timer / PERIOD) % 7.0) / 7.0 * 0.5 + 0.5)) for v in color)
-        for y in range(4):
-            col = (lambda c, w: tuple(c[i] * COLOR_RATIO + w[i] * (1 - COLOR_RATIO) for i in (0, 1, 2)))(
-                color,
-                _wheel(((x + y) * 255.0 / 16 + timer * 255.0 / PERIOD) % 256),
-            )
+        for y in range(len(pixels) // 8):
+            rainbow_color = _wheel(((x + y) * 255.0 / 16 + timer * 255.0 / PERIOD) % 256)
+            col = mix_colors(color, rainbow_color)
+            if (
+                not switch.value and (y == 3 or (x, y) in ((6, 2), (5, 1), (4, 0)))
+                or switch.value and (y == 0 or (x, y) in ((1, 1), (2, 2), (3, 3)))
+            ):
+                col = tuple(c // 6 for c in color)  # mix_colors(rainbow_color, col)
             pixels[y * 8 + x] = col
     # pixels[0] = (255,0,0)
     # pixels[1] = (0,255,0)
     pixels.show()
+    if MAIN:
+        pixel[0] = pixels[0]
+        pixel.show()
 
     timer = (timer + 1) % PERIOD
 
+if MAIN:
+    update_display("primary")
 
-v = pressed = pressed_now = got_input = swin = switch_to_1 = switch_to_0 = switch_to_1_now = switch_to_0_now = False
+pressed = pressed_now = got_input = swin = switch_to_1 = switch_to_0 = switch_to_1_now = switch_to_0_now = False
+v = button.value if MAIN else None
 while True:
     time.sleep(0.01)
 
@@ -124,22 +178,12 @@ while True:
                 print("switch_to_1_now")
     else:
         got_input = False
-
-    if IS_CPE:
-        pressed_now = button_a.value or button_b.value
-        if not pressed and pressed_now:
-            v = not v
-            print(v)
-            while button_a.value or button_b.value:
-                time.sleep(0.1)
-        pressed = pressed_now
-    elif MAIN:
+    
+    if MAIN:
         pressed_now = button.value
         if pressed_now != pressed:
             v = not v
         pressed = pressed_now
-
-    if MAIN:
         switch_to_0_now = switch_in.value
         if switch_to_0_now and not switch_to_0:
             v = False
@@ -148,8 +192,11 @@ while True:
         if switch_to_1_now and not switch_to_1:
             v = True
         switch_to_1 = switch_to_1_now
-        update_pixels(RED if got_input else BLUE if v else PURPLE)
+        is_update = v != switch.value
         switch.value = v
+        update_pixels(RED if got_input else BLUE if v else PURPLE)
+        if is_update:
+            update_display("secondary" if v else "primary")
 
     if WORK:
         v = switch_to_1_now
